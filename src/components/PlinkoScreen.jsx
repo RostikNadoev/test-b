f621d8aba086ae3afe11995e133e7e2b3a41ba3e
Your Name <you@example.com>
2026-02-13 20:02:07 +0300

    clone4

--- src/components/PlinkoScreen.jsx
+++ src/components/PlinkoScreen.jsx
@@ -38,7 +38,7 @@ export default function PlinkoScreen({ onNavigate }) {
               className="plinko-drop-btn"
               onClick={handleDropBall}
             >
-              DROP bALL1111
+              DROP bALL1111111
             </button>
           </div>
         </div>
