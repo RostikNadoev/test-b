ad965661d02ac94497b1fbdeb3ce6eb17c9d29fd
Your Name <you@example.com>
2026-03-13 21:54:18 +0300

    rocketnew9

--- src/hooks/useCrashGame.js
+++ src/hooks/useCrashGame.js
@@ -154,7 +154,7 @@ export const useCrashGame = () => {
         setMyActiveBet(null);
         myActiveBetRef.current = null;
         clearTableTimeoutRef.current = null;
-      }, 2000);
+      }, 1920);
     }
     
     return () => {
